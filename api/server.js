90973c5a1c4e603df8f67ffe58f22a16463924ac
John Kakuk <john@Katana.local>
2026-03-05 19:58:30 -0800

    Basic CRA build

--- api/server.js
+++ api/server.js
@@ -19,7 +19,15 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log("Database Connection Established"))
 
 app.use(express.json())
-app.use('/students', studentRouter)
+app.use('/api/v1/students', studentRouter)
+
+// Look for static build
+app.use(express.static(path.join(__dirname, '../reactjs/build')));
+
+// For any routes not defined by API, assume direct request to client-side route
+app.get('/{*splat}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../reactjs/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
