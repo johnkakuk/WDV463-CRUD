025893bd787301feb8d00b80d6b8c503f140c614
John Kakuk <john@Katana.local>
2026-03-05 16:40:18 -0800

    Init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+const studentRouter = require('./routes/students')
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log("Database Connection Established"))
+
+app.use(express.json())
+app.use('/students', studentRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
